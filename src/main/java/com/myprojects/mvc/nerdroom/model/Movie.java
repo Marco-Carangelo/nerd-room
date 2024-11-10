78a3365513ac15500ce9c03c95c48e9f63df22c0
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-11-10 19:37:30 +0100

    Update: Movie attributes getters and setters added

--- src/main/java/com/myprojects/mvc/nerdroom/model/Movie.java
+++ src/main/java/com/myprojects/mvc/nerdroom/model/Movie.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.util.Date;
@@ -39,6 +40,51 @@ public class Movie {
 	@Past
 	@Column
 	private Date year;
+	
+	
+	//Getters and setters
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public Date getYear() {
+		return year;
+	}
+
+	public void setYear(Date year) {
+		this.year = year;
+	}
+	
+	
 	
 	
 	
