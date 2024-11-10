7fd300c26cfa733dc837b77a8070fdeba5e9e8ef
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-11-10 19:36:21 +0100

    Update: Movie attributes added

--- src/main/java/com/myprojects/mvc/nerdroom/model/Movie.java
+++ src/main/java/com/myprojects/mvc/nerdroom/model/Movie.java
@@ -1,10 +1,48 @@
 package com.myprojects.mvc.nerdroom.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
 
 @Entity
 @Table(name = "movies")
 public class Movie {
+	
+	//Definition of the class attributes
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Size(min = 2, max = 50)
+	@NotBlank
+	@Column
+	private String title;
+	
+	
+	@Size(min = 5, max = 1000)
+	@Column
+	private String description;
+	
+	@Size(min = 2, max = 50)
+	@NotBlank
+	@Column
+	private String genre;
+	
+	@Past
+	@Column
+	private Date year;
+	
+	
+	
+	
+	
 
 }
