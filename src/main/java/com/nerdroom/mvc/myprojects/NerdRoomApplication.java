8a5d99526abad476e1ac20b66520da0e0b86a3cb
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-11-10 17:17:27 +0100

    Initial commit

---
+++ src/main/java/com/nerdroom/mvc/myprojects/NerdRoomApplication.java
@@ -0,0 +1,13 @@
+package com.nerdroom.mvc.myprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerdRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nerdroom/mvc/myprojects/NerdRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerdroom.mvc.myprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerdRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
