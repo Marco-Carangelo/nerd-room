71e7e26049dbf32835f218dafd76d94a5ca9de89
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-11-13 12:06:53 +0100

    Create: new service class MovieService

---
+++ src/main/java/com/myprojects/mvc/nerdroom/service/MovieService.java
@@ -0,0 +1,14 @@
+package com.myprojects.mvc.nerdroom.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.myprojects.mvc.nerdroom.repository.MovieRepository;
+
+@Service
+public class MovieService {
+	
+	@Autowired
+	private MovieRepository repo;
+
+}
