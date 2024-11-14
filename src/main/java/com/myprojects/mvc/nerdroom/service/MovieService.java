f946890f11fe397dea2df8eb525cf63a5fd92e8c
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-11-14 17:30:45 +0100

    add:  new method findMovies that return a list with all the movies

--- src/main/java/com/myprojects/mvc/nerdroom/service/MovieService.java
+++ src/main/java/com/myprojects/mvc/nerdroom/service/MovieService.java
@@ -1,8 +1,11 @@
 package com.myprojects.mvc.nerdroom.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.myprojects.mvc.nerdroom.model.Movie;
 import com.myprojects.mvc.nerdroom.repository.MovieRepository;
 
 @Service
@@ -10,5 +13,9 @@ public class MovieService {
 	
 	@Autowired
 	private MovieRepository repo;
+	
+	public List<Movie> findMovies(){
+		return repo.findAll();
+	}
 
 }
