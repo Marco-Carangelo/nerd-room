4060f1e6d6a64872c66432ce566cf26f4776f6c3
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-11-14 17:32:19 +0100

    add: new method that return the index template for Movie

--- src/main/java/com/myprojects/mvc/nerdroom/controller/MovieController.java
+++ src/main/java/com/myprojects/mvc/nerdroom/controller/MovieController.java
@@ -1,11 +1,28 @@
 package com.myprojects.mvc.nerdroom.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.myprojects.mvc.nerdroom.service.MovieService;
 
 @Controller
 @RequestMapping("/movies")
 public class MovieController {
+	
+	//Service classes
+	@Autowired
+	private MovieService movieService;
+	
+	@GetMapping
+	public String index(Model model ) {
+		
+		model.addAttribute("movieList", movieService.findMovies());
+		return "/movies/index";
+		
+	}
 	
 
 }
