afdaa26cbf9c6640053dcdec6bd15524c476cbd1
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-11-15 19:09:23 +0100

    add: new get method create

--- src/main/java/com/myprojects/mvc/nerdroom/controller/MovieController.java
+++ src/main/java/com/myprojects/mvc/nerdroom/controller/MovieController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.myprojects.mvc.nerdroom.model.Movie;
 import com.myprojects.mvc.nerdroom.service.MovieService;
 
 @Controller
@@ -31,8 +32,18 @@ public class MovieController {
 	public String show(Model model, @PathVariable("id") Integer id ) {
 		
 		model.addAttribute("movie", movieService.findMovieById(id));
-		
 		return "/movies/show";
+	}
+	
+	//Create Method
+	@GetMapping("/create")
+	public String create(Model model){
+		
+		//Add to model a black movie object
+		Movie movie = new Movie();
+		model.addAttribute("movie", movie);
+		
+		return "/movies/create";
 	}
 	
 
