f15a6fc5cf3122601f749ff304afa0219e9e441a
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-11-15 16:08:37 +0100

    add: new show method mapping "movies/{id}"

--- src/main/java/com/myprojects/mvc/nerdroom/controller/MovieController.java
+++ src/main/java/com/myprojects/mvc/nerdroom/controller/MovieController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.myprojects.mvc.nerdroom.service.MovieService;
@@ -16,12 +17,22 @@ public class MovieController {
 	@Autowired
 	private MovieService movieService;
 	
+	//Index Method
 	@GetMapping
 	public String index(Model model ) {
 		
 		model.addAttribute("movieList", movieService.findMovies());
 		return "/movies/index";
 		
+	}
+	
+	//Show Method
+	@GetMapping("/{id}")
+	public String show(Model model, @PathVariable("id") Integer id ) {
+		
+		model.addAttribute("movie", movieService.findMovieById(id));
+		
+		return "/movies/show";
 	}
 	
 
