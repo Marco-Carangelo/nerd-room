00ffc9c0faf1f0fee3edd730591ca6e4c9bf9d5e
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-11-17 12:57:04 +0100

    Add: new store method to save the new Movie objects

--- src/main/java/com/myprojects/mvc/nerdroom/controller/MovieController.java
+++ src/main/java/com/myprojects/mvc/nerdroom/controller/MovieController.java
@@ -1,14 +1,23 @@
 package com.myprojects.mvc.nerdroom.controller;
 
+import java.time.Year;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.myprojects.mvc.nerdroom.model.Movie;
 import com.myprojects.mvc.nerdroom.service.MovieService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/movies")
@@ -46,5 +55,25 @@ public class MovieController {
 		return "/movies/create";
 	}
 	
-
+	@PostMapping("/create")
+	public String store(
+			@Valid @ModelAttribute("movie") Movie formMovie,
+			BindingResult bindingResult,
+			Model model,
+			RedirectAttributes attributes,
+			@RequestParam(name = "yearValue") int yearValue) {
+		
+		//Set the given year as value of the Year object
+		Year formYear = Year.of(yearValue);
+		formMovie.setYear(formYear);
+		
+		//Check if the object has errors
+		if(bindingResult.hasErrors()) {
+			return "/movies/create";
+		}
+		
+		movieService.createMovie(formMovie);
+		
+		return "redirect:/movies";
+	}
 }
