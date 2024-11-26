649c89668fa09a9f081d48ec588765a1a5c9df7f
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-11-26 23:25:05 +0100

    Add:  getters and setters

--- src/main/java/com/myprojects/mvc/nerdroom/model/Attribute.java
+++ src/main/java/com/myprojects/mvc/nerdroom/model/Attribute.java
@@ -23,4 +23,22 @@ public class Attribute {
 		@Column
 		private String name;
 
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		
+
 }
