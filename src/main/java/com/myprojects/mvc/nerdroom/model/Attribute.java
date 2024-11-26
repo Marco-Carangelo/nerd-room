4361dc804c8c4fb3191bbdb5c5a3c23081f28fcb
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-11-26 23:22:02 +0100

    add: Attribute class fields

--- src/main/java/com/myprojects/mvc/nerdroom/model/Attribute.java
+++ src/main/java/com/myprojects/mvc/nerdroom/model/Attribute.java
@@ -1,10 +1,26 @@
 package com.myprojects.mvc.nerdroom.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "attributes")
 public class Attribute {
+	
+	//Definition of the class attributes
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Integer id;
+		
+		@Size(min = 2, max = 50)
+		@NotBlank
+		@Column
+		private String name;
 
 }
