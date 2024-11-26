81f902954aa5f49ea0ff0d0cff6201c603d5d0ca
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-11-26 23:57:52 +0100

    Create: new repository interface MediaAttributeRepository

---
+++ src/main/java/com/myprojects/mvc/nerdroom/repository/MediaAttributeRepository.java
@@ -0,0 +1,10 @@
+package com.myprojects.mvc.nerdroom.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.myprojects.mvc.nerdroom.model.MediaAttribute;
+
+
+public interface MediaAttributeRepository extends JpaRepository<MediaAttribute, Integer>{
+
+}
