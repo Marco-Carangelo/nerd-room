2538c4de37291970cea33217a03db9b313c42027
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-11-28 23:56:06 +0100

    add: JPA annotation added to the class

--- src/main/java/com/myprojects/mvc/nerdroom/model/Media.java
+++ src/main/java/com/myprojects/mvc/nerdroom/model/Media.java
@@ -2,15 +2,48 @@ package com.myprojects.mvc.nerdroom.model;
 
 import java.time.Year;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Media {
 	
-	//Clas attributes
+	//Class attributes
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Size(min = 2, max = 50)
+	@NotBlank
+	@Column
 	private String title;
+	
+	@Size(min = 5, max = 1000)
+	@Column
 	private String description;
+	
+	@PastOrPresent
+	@Column
 	private Year year;
 	
 	//Getters and setters
-	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
 	public String getTitle() {
 		return title;
 	}
