19b7d65eccf62bf7c11de9bd9f0748456dd30d3f
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-11-29 02:20:20 +0100

    Update: Movie become subclass of Media

--- src/main/java/com/myprojects/mvc/nerdroom/model/Movie.java
+++ src/main/java/com/myprojects/mvc/nerdroom/model/Movie.java
@@ -1,76 +1,28 @@
 package com.myprojects.mvc.nerdroom.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Size;
 
-import java.time.Year; 
-
-import jakarta.persistence.Column;
-
 @Entity
-@Table(name = "movies")
-public class Movie {
+@DiscriminatorValue("movie")
+public class Movie extends Media {
 	
 	//Definition of the class attributes
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	
-	@Size(min = 2, max = 50)
-	@NotBlank
-	@Column
-	private String title;
-	
+
 	@Size(min = 2, max = 50)
 	@NotBlank
 	@Column
 	private String director;
-	
-	@Size(min = 5, max = 1000)
-	@Column
-	private String description;
 	
 	@Size(min = 2, max = 50)
 	@NotBlank
 	@Column
 	private String genre;
 	
-	@PastOrPresent
-	@Column
-	private Year year;
-	
-	
 	//Getters and setters
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
 
 	public String getGenre() {
 		return genre;
@@ -78,14 +30,6 @@ public class Movie {
 
 	public void setGenre(String genre) {
 		this.genre = genre;
-	}
-
-	public Year getYear() {
-		return year;
-	}
-
-	public void setYear(Year year) {
-		this.year = year;
 	}
 
 	public String getDirector() {
