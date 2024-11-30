dc2e7893a32294ec89c8614d016c48b1dd0dbcfa
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-11-30 14:40:57 +0100

    Add: Many to one relationship with Media class

--- src/main/java/com/myprojects/mvc/nerdroom/model/Rating.java
+++ src/main/java/com/myprojects/mvc/nerdroom/model/Rating.java
@@ -2,6 +2,8 @@ package com.myprojects.mvc.nerdroom.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -10,6 +12,16 @@ import jakarta.validation.constraints.NotNull;
 @Entity
 @Table(name = "ratings")
 public class Rating {
+	
+	@Max(100)
+	@Min(0)
+	@NotNull
+	@Column
+	private Integer value = 0;
+	
+	@ManyToOne
+	@JoinColumn(name = "media_id", nullable = false)
+	private Media media;
 	
 	
 
