8cac69e54a2affad007dcd80d73aff3e33c48ec2
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-11-30 18:51:36 +0100

    Add: new store method for MediaAttribute

--- src/main/java/com/myprojects/mvc/nerdroom/controller/MediaAttributeController.java
+++ src/main/java/com/myprojects/mvc/nerdroom/controller/MediaAttributeController.java
@@ -2,9 +2,17 @@ package com.myprojects.mvc.nerdroom.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.myprojects.mvc.nerdroom.model.MediaAttribute;
 import com.myprojects.mvc.nerdroom.service.MediaAttributeService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/media-attributes")
@@ -13,5 +21,27 @@ public class MediaAttributeController {
 	//Service Classes
 	@Autowired
 	private MediaAttributeService mediaAttributeService;
+	
+	//CRUD methods
+	
+	//Create method
+	@PostMapping("/media-attributes")
+	public String store(
+			@Valid @ModelAttribute("mediaAttribute") MediaAttribute formMediaAttribute,
+			BindingResult bindingResult,
+			Model model,
+			RedirectAttributes attributes
+			) {
+		
+		//Check if the object has errors
+		if(bindingResult.hasErrors()) {
+			return "/media-attributes";
+		}
+		
+		mediaAttributeService.createMediaAttribute(formMediaAttribute);
+		
+		return "redirect:/media-attributes";
+	}
+	
 	
 }
