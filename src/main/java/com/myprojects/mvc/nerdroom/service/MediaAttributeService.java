a0776dec95b5ec84fad4346c90375beb0e57024e
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-11-30 18:50:53 +0100

    Add: new create service method for MediaAttribute

--- src/main/java/com/myprojects/mvc/nerdroom/service/MediaAttributeService.java
+++ src/main/java/com/myprojects/mvc/nerdroom/service/MediaAttributeService.java
@@ -3,13 +3,19 @@ package com.myprojects.mvc.nerdroom.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.myprojects.mvc.nerdroom.model.MediaAttribute;
 import com.myprojects.mvc.nerdroom.repository.MediaAttributeRepository;
 
 @Service
 public class MediaAttributeService {
 	
 	//Injection of the repository interface
-		@Autowired
-		private MediaAttributeRepository repo;
+	@Autowired
+	private MediaAttributeRepository repo;
+		
+	//Service methods
+	public MediaAttribute createMediaAttribute(MediaAttribute mediaAttribute) {
+		return repo.save(mediaAttribute);
+	}
 
 }
