95d3487d9260b6a516864febf1d5fb80822f5225
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-11-30 14:33:18 +0100

    Crate: new entity class Rating.java

---
+++ src/main/java/com/myprojects/mvc/nerdroom/model/Rating.java
@@ -0,0 +1,16 @@
+package com.myprojects.mvc.nerdroom.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "ratings")
+public class Rating {
+	
+	
+
+}
