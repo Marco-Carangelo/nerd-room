daa440caca69be9baf59cdd207caaa2dd1d83963
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-12-01 20:29:13 +0100

    Add: index method for the MediaAttribute entity

--- src/main/java/com/myprojects/mvc/nerdroom/controller/MediaAttributeController.java
+++ src/main/java/com/myprojects/mvc/nerdroom/controller/MediaAttributeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,15 @@ public class MediaAttributeController {
 	private MediaAttributeService mediaAttributeService;
 	
 	//CRUD methods
+	
+	//Index method
+	@GetMapping
+	public String index(Model model) {
+		
+		model.addAttribute("mediaAttributesList", mediaAttributeService.findMediaAttributes());
+		
+		return "/media-attributes/index";
+	}
 	
 	//Create method
 	@PostMapping("/media-attributes")
