af0f66564976f337470d0d3ad042c9eca2320bae
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-12-04 02:20:15 +0100

    Add: new custom query method to find movies by substring

--- src/main/java/com/myprojects/mvc/nerdroom/repository/MovieRepository.java
+++ src/main/java/com/myprojects/mvc/nerdroom/repository/MovieRepository.java
@@ -1,9 +1,14 @@
 package com.myprojects.mvc.nerdroom.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.myprojects.mvc.nerdroom.model.Movie;
 
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
+	
+	//JPA custom query methods
+	public List<Movie> findByTitleContainsIgnoreCase(String text);
 
 }
