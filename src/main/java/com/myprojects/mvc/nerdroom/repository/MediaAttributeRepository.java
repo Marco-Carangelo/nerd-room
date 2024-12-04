3853fd61132d002f5ea3a9ed148056cf56d7f4b8
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-12-04 02:11:16 +0100

    add: new custom query to find media attributes by name

--- src/main/java/com/myprojects/mvc/nerdroom/repository/MediaAttributeRepository.java
+++ src/main/java/com/myprojects/mvc/nerdroom/repository/MediaAttributeRepository.java
@@ -1,10 +1,15 @@
 package com.myprojects.mvc.nerdroom.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.myprojects.mvc.nerdroom.model.MediaAttribute;
 
 
 public interface MediaAttributeRepository extends JpaRepository<MediaAttribute, Integer>{
+	
+	//Repository custom methods
+	public List<MediaAttribute> findByNameStartingWithIgnoreCase(String text);
 
 }
