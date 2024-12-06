c9e978913a6a087624b23fd7b9c4295a1a3803e3
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-12-06 19:53:54 +0100

    add: createRating service method

--- src/main/java/com/myprojects/mvc/nerdroom/service/RatingService.java
+++ src/main/java/com/myprojects/mvc/nerdroom/service/RatingService.java
@@ -3,6 +3,7 @@ package com.myprojects.mvc.nerdroom.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.myprojects.mvc.nerdroom.model.Rating;
 import com.myprojects.mvc.nerdroom.repository.RatingRepository;
 
 @Service
@@ -10,4 +11,11 @@ public class RatingService {
 
 	@Autowired
 	private RatingRepository repo;
+	
+	//CRUD methods
+	
+	//Create method
+	public Rating createRating(Rating rating) {
+		return repo.save(rating);
+	}
 }
