5ad5a66e09c7c6dcc47624b2f3417eb875356ea3
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-12-06 20:06:30 +0100

    Add: create method for Rating obj mepping 'ratings/create'

--- src/main/java/com/myprojects/mvc/nerdroom/controller/RatingController.java
+++ src/main/java/com/myprojects/mvc/nerdroom/controller/RatingController.java
@@ -1,9 +1,17 @@
 package com.myprojects.mvc.nerdroom.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.myprojects.mvc.nerdroom.model.Rating;
 import com.myprojects.mvc.nerdroom.service.RatingService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/ratings")
@@ -11,5 +19,27 @@ public class RatingController {
 	
 	//Service classes
 	private RatingService ratingService;
+	
+	//CRUD methods
+	
+	//Create method
+	@PostMapping("/create")
+	public String store(
+			@Valid @ModelAttribute("rating") Rating formRating,
+			BindingResult bindingResult,
+			Model model,
+			RedirectAttributes attributes
+			) {
+		
+		//Check if the formRating object has not errors and save the Rating before to return to the media page
+		if(!bindingResult.hasErrors()) {	
+			ratingService.createRating(formRating);
+		}
+		return "movies/" + formRating.getMedia().getId();
+	}
+	
+	//Update method
+	
+	//Delete method
 
 }
