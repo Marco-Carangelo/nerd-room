96a2323f455abd813c1172e1eb1b9aedba5d8ada
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-12-07 16:53:11 +0100

    Create: new class service CategoryService

---
+++ src/main/java/com/myprojects/mvc/nerdroom/service/CategoryService.java
@@ -0,0 +1,11 @@
+package com.myprojects.mvc.nerdroom.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repo;
+
+}
