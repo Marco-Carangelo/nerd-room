3d925553f137d6283a6a8e584f9663aba504de21
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-12-07 15:44:06 +0100

    Add: OneToMany relation with MediaAttribute

--- src/main/java/com/myprojects/mvc/nerdroom/model/AttributeCategory.java
+++ src/main/java/com/myprojects/mvc/nerdroom/model/AttributeCategory.java
@@ -1,10 +1,14 @@
 package com.myprojects.mvc.nerdroom.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -14,30 +18,45 @@ import jakarta.validation.constraints.Size;
 public class AttributeCategory {
 
 	//Definition of the class attributes
-			@Id
-			@GeneratedValue(strategy = GenerationType.IDENTITY)
-			private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Size(min = 2, max = 50)
+	@NotBlank
+	@Column
+	private String name;
+	
+	//Relationship mapping fields
+	
+	@OneToMany(mappedBy = "attribute-categories", cascade = {CascadeType.REMOVE})
+	private List<MediaAttribute> mediaAttributes;
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<MediaAttribute> getMediaAttributes() {
+		return mediaAttributes;
+	}
+
+	public void setMediaAttributes(List<MediaAttribute> mediaAttributes) {
+		this.mediaAttributes = mediaAttributes;
+	}
 			
-			@Size(min = 2, max = 50)
-			@NotBlank
-			@Column
-			private String name;
-
-			public Integer getId() {
-				return id;
-			}
-
-			public void setId(Integer id) {
-				this.id = id;
-			}
-
-			public String getName() {
-				return name;
-			}
-
-			public void setName(String name) {
-				this.name = name;
-			}
-			
+	
 			
 }
