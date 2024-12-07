86c965bd06f5099cd9e05e4e3b76a72c21a2dc92
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-12-07 16:57:07 +0100

    Add: index, create and delete methods for category entity

--- src/main/java/com/myprojects/mvc/nerdroom/service/CategoryService.java
+++ src/main/java/com/myprojects/mvc/nerdroom/service/CategoryService.java
@@ -1,11 +1,33 @@
 package com.myprojects.mvc.nerdroom.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.myprojects.mvc.nerdroom.model.Category;
+import com.myprojects.mvc.nerdroom.repository.CategoryRepository;
 
 @Service
 public class CategoryService {
 	
 	@Autowired
 	private CategoryRepository repo;
+	
+	
+	//CRUD METHODS
+	
+	public List<Category> findCategories(){
+		return repo.findAll();
+	}
+	
+	public Category createCategory(Category category){
+		return repo.save(category);
+	}
+	
+	public void deleteCategory(Integer id) {
+		repo.deleteById(id);
+	}
+	
 
 }
