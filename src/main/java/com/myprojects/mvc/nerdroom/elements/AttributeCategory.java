ad0e85bf6d4c9b1e2abea6b5faeb34b63207dcf5
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-12-07 13:51:08 +0100

    Fix: enum class name changed to AttributeCategory

--- src/main/java/com/myprojects/mvc/nerdroom/elements/AttributeCategory.java
+++ src/main/java/com/myprojects/mvc/nerdroom/elements/AttributeCategory.java
@@ -1,7 +1,7 @@
 package com.myprojects.mvc.nerdroom.elements;
 
 //Enum object that contains the various categories for media attributes 
-public enum AttributeCategories {
+public enum AttributeCategory {
 	FILM_MAKING,
 	WRITING,
 	VISUAL,
