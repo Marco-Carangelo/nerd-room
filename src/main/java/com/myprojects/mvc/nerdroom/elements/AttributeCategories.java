4979ab52c23e0efd17b7dfa04d30ad24241e562b
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-12-07 13:46:04 +0100

    Create: new enum class containing media attributes categories

---
+++ src/main/java/com/myprojects/mvc/nerdroom/elements/AttributeCategories.java
@@ -0,0 +1,14 @@
+package com.myprojects.mvc.nerdroom.elements;
+
+//Enum object that contains the various categories for media attributes 
+public enum AttributeCategories {
+	FILM_MAKING,
+	WRITING,
+	VISUAL,
+	ENTERTAIMENT,
+	INTROSPECTION,
+	FEELINGS,
+	ACTING,
+	OTHERS
+
+}
