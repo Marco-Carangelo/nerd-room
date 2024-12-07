2ab3a63c5b97833bb3d31c662017b758b8eca6c9
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-12-07 15:30:38 +0100

    Create: new model entity class named AttributeCategory

---
+++ src/main/java/com/myprojects/mvc/nerdroom/model/AttributeCategory.java
@@ -0,0 +1,43 @@
+package com.myprojects.mvc.nerdroom.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "attribute-categories")
+public class AttributeCategory {
+
+	//Definition of the class attributes
+			@Id
+			@GeneratedValue(strategy = GenerationType.IDENTITY)
+			private Integer id;
+			
+			@Size(min = 2, max = 50)
+			@NotBlank
+			@Column
+			private String name;
+
+			public Integer getId() {
+				return id;
+			}
+
+			public void setId(Integer id) {
+				this.id = id;
+			}
+
+			public String getName() {
+				return name;
+			}
+
+			public void setName(String name) {
+				this.name = name;
+			}
+			
+			
+}
