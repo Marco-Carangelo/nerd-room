42104db40c895b59ff0174d8eb8ff028ae54aa8c
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-12-10 23:54:31 +0100

    Add: new on click event for editBtn element

--- src/main/resources/static/js/movies/movieShowScript.js
+++ src/main/resources/static/js/movies/movieShowScript.js
@@ -4,3 +4,13 @@ const commentDisplay = document.getElementById('comment-display');
 const editBtn = document.getElementById('edit-btn');
 const formMovie = document.getElementById('form-movie');
 
+
+//Function to change visibility to paragraph and text area of the comment section when the edit button is clicked
+editBtn.addEventListener('click', function(event){
+	event.preventDefault();
+	
+	//Hide the paragraph element and show the form with textarea to consent the text editing
+	commentDisplay.classList.add('visually-hidden');
+	formMovie.classList.remove('visually-hidden');
+	
+})
