839d47e42eb7fe1e3d89cb069a6eb8c9b27734d1
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-12-10 23:15:34 +0100

    Create: new js file movieShowScript

---
+++ src/main/resources/static/js/movies/movieShowScript.js
@@ -0,0 +1,6 @@
+//Get the element from the dom
+
+const commentDisplay = document.getElementById('comment-display');
+const editBtn = document.getElementById('edit-btn');
+const formMovie = document.getElementById('form-movie');
+
